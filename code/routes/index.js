aae6ef1182043998392c5c34061bc2371c081747
Matt Cook <matthew.cook1994@outlook.com>
2018-07-23 14:02:35 +1000

    Search functionality added

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,40 +1,64 @@
 var express = require('express');
 var router = express.Router();
-var db1 = require('../dbconnection');
-var db = require('../models/properties');
+var db = require('../dbconnection');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-/*   var query = db1.query('SELECT * FROM properties_sample',function(err, result, fields) {
-        if (err) throw err; */
-  
   res.render('index', { 
     title: 'Rezone',
-    properties: db.query
   });
 });
 
-/*  */
-router.get('/:query', function(req, res, next) {
-  res.render('index', { title: 'Rezone', output: req.params.query});
-});
 
-/*  */
-router.post('/submit', function(req, res, next) {
-  var lotplan = req.body.lotplan;
-  var query = db1.query(
-    "SELECT * FROM properties_sample WHERE lotplan = '215SP192046'",function(err, result, fields) {
-        if (err) throw err;
-        if(result.length > 0) {
-            query = result;
-        } else {
-            query = null;
-        }
-        //query = JSON.stringify(query);
-        console.log('Test result: ', query);        //Result is an object containing properties
-});
-  res.redirect('/' + query);
+/*  Fetch individual lot & property address*/
+router.get('/searchaddress', function(req, res, next) {
+  var a = req.query.address;
+  console.log("Address found:" + a);
+  var sql = `SELECT * FROM address_sample WHERE address = '${a}'`;
+  var query = db.query(sql,function(err, addressresult, fields) {
+      if (err) throw err; 
+       //query = JSON.stringify(query);
+       console.log('Address fetched: ', addressresult);        //Result is the object
+       var lotplanreference = addressresult[0].LOTPLAN;
+       var sql = `SELECT * FROM properties_sample WHERE lotplan = '${lotplanreference}'`;
+       var query = db.query(sql,function(err, lotplanresult, fields) {
+          if (err) throw err; 
+          //query = JSON.stringify(query);
+          console.log('Lotplan fetched: ', lotplanresult);
+          res.render('index', { 
+          title: 'Rezone',
+          address: addressresult[0].ADDRESS,
+          cityplace: addressresult[0].CITY_PLACE,
+          state: addressresult[0].STATE,
+          lotplan: addressresult[0].LOTPLAN,
+          wkt2: lotplanresult[0].WKT,
+          lot_area: lotplanresult[0].LOT_AREA
+          });
+      });
+  });
+  
 });
 
 
 module.exports = router;
+
+
+/*  Fetch individual LOTPLAN address*/
+/*  router.get('/searchlotplan', function(req, res, next) {
+   var a = req.query.q;
+
+   var sql = `SELECT * FROM properties_sample WHERE lotplan = '${a}'`;
+   var query = db.query(sql,function(err, result, fields) {
+       if (err) throw err; 
+        //query = JSON.stringify(query);
+        console.log('Lotplan fetched: ', result);        //Result is the object
+        res.render('index', { 
+         title: 'Rezone',
+         lot: result[0].LOT,
+         lot_area: result[0].LOT_AREA,
+         locality: result[0].LOCALITY,
+         wkt: result[0].WKT
+      });
+   });
+}); */
